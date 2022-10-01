a8f13023b9de993e620790a1f6df65064452a40d
sushmavarma7 <46321720+sushmavarma7@users.noreply.github.com>
2022-10-01 17:58:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """session = boto3.Session(
-aws_access_key_id = 'AKIATOBQW6JXZW2N6SGC'
-aws_secret_access_key = '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'
+aws_access_key_id = '************'
+aws_secret_access_key = '*********'
 )
-s3 = session.resource('s3')
-import boto3"""
+s3 = session.resource('s3')"""
+
 import boto3
 client = boto3.client('s3')
 response = client.list_objects_v2(
